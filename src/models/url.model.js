ad801f16c9709dd48bf40bf364cf0d3035151333
Noman <pxycknomdictator@outlook.com>
2024-09-14 01:26:35 +0500

    added url model in codebase

---
+++ src/models/url.model.js
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+const urlSchema = new Schema(
+  {
+    originalUrl: {
+      type: String,
+      required: true,
+    },
+    shortId: {
+      type: String,
+      required: true,
+    },
+    shortUrl: {
+      type: String,
+      required: true,
+    },
+    visit_history: {
+      type: Number,
+      default: 0,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Urls = model("Urls", urlSchema);
