cfc463b390d2b200a3daf4beed3a89cee6487682
Noman <pxycknomdictator@outlook.com>
2024-09-14 14:03:18 +0500

    added database connection

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import { _configs } from "./constants/constant.js";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/user.route.js";
 import urlRouter from "./routes/url.route.js";
+import { DB_CONNECTION } from "./config/db.connection.js";
 
 const app = express();
 const PORT = _configs.port || 8000;
+
+// Database connection
+DB_CONNECTION()
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Started at http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log("Error", error?.message));
 
 // Json parsers || Middlewares
 app.use(urlencoded({ extended: false, limit: "20kb" }));
@@ -20,8 +30,3 @@ app.set("views", path.resolve("./src/views"));
 // auth routes
 app.use("/auth", authRouter);
 app.use("/", urlRouter);
-
-// Listening on port
-app.listen(PORT, () =>
-  console.log(`Server Started at http://localhost:${PORT}`)
-);
